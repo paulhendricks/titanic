ace302af2bce5b9261ac84503617c2ab9788edbf
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-28 18:00:19 +0000

    update with source

--- R/titanic_gender_model.R
+++ R/titanic_gender_model.R
@@ -2,7 +2,7 @@
 #'
 #' Description.
 #'
-#' @source http://www.something.com
+#' @source https://www.kaggle.com/c/titanic/data
 #' @format Data frame with columns
 #' \describe{
 #' \item{carrier}{Two letter abbreviation}
